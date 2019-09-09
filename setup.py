5122a4474126e2ee4fc08d3441a451b31cce7900
creme <creme@envs.net>
2019-09-09 18:51:38 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         'License :: OSI Approved :: MIT License',
     ],
     keywords='blog',
-    packages=setuptools.find_packages(),
+    packages=[ttbp],
     install_requires = [
         'inflect==0.2.5',
         'mistune==0.8.1',
